ff4b43a566856bce73f64ecab39f09d3cbeb670e
Kristie Kim <kristiekim2009@gmail.com>
2025-09-05 22:45:19 +1000

    Update PharmAssist.py

--- PharmAssist.py
+++ PharmAssist.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="PharmAssist",
-    page_icon="💊",
+    page_icon="PharmAssist Favi.ico",
     layout="centered",
     initial_sidebar_state="expanded",
 )
