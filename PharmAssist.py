363962f3d7d9791dcbaadbb46b7a359a80e80249
Kristie Kim <kristiekim2009@gmail.com>
2025-09-15 00:50:30 +1000

    Update PharmAssist.py

--- PharmAssist.py
+++ PharmAssist.py
@@ -9,30 +9,30 @@ st.set_page_config(
 
 st.title("PharmAssist")
 st.write("Your Over-the-Counter (OTC) medication recommender")
-st.caption("Please note that PharmBot is not a substitute for professional medical advice. Always consult a " \
-"healthcare provider when experiencing serious symptoms, in pregnancy, or taking other medications.")
+st.caption("Please note that PharmBot is not a substitute for professional medical advice. Always confirm with your pharmacist before purchasing an OTC medication!" \
+"Be sure to consult a healthcare provider if you are experiencing serious symptoms, are pregnant, or taking other medications.")
 
 symptom = st.text_area("Describe your symptoms:")
 
-msg = "Would you like me to find a nearby pharmacy for you?"
-errorMsg = "I'm sorry, I couldn't understand your symptoms. Consider consulting a healthcare professional for accurate advice."
+msg = "Remember to always consult your pharmacist or check the product label for personal appropriate dosages! \
+Would you like me to find a nearby pharmacy for you?"
+errorMsg = "I'm sorry, I couldn't understand your symptoms. Consider consulting a healthcare professional for more accurate advice."
 
 medDict = {
-    ("headache","migrane","headaches","head ache","head aches","head pain","head","head pains"): ["Tylenol", "Ibuprofen (Advil)"],
-    ("cough","sore throat","coughing","coughs"): ["Dextromethorphan (Robitussin)", "Honey-based syrups"],
-    ("allergy","allergies","allergic"): ["Loratadine (Claritin)", "Cetirizine (Zyrtec)"],
-    ("fever","sick"): ["Acetaminophen (Tylenol)", "Ibuprofen (Advil)"],
-    ("cold","sick","sickness"): ["Decongestants (Sudafed)", "Saline nasal sprays"],
-    "nausea": ["Bismuth subsalicylate (Pepto-Bismol)", "Ginger supplements"],
-    "diarrhea": ["Loperamide (Imodium)", "Oral rehydration solutions"],
-    ("constipation", "constipated","constipate"): ["Fiber supplements", "Stool softeners (Colace)"],
-    ("heartburn","heart burn","indigestion", "indigest","sour","sour stomach"): ["Antacids (Tums)", "H2 blockers (Ranitidine)"],
-    ("runny nose","runny"): ["Antihistamines (Claritin)", "Nasal corticosteroids (Flonase)"],
-    ("sneezing", "sneeze"): ["Antihistamines (Claritin)", "Nasal corticosteroids (Flonase)"],
-    ("congestion","stuffy", "stuffed nose", "stuffy nose", "nose congestion", "congest"): ["Decongestants (Sudafed)", "Saline nasal sprays"],
-    ("muscle pain", "sore muscle", "sore muscles", "muscle sore", "muscle sores"): ["Acetaminophen (Tylenol)", "Ibuprofen (Advil)"],
-    ("toothache", "tooth pain", "tooth", "teethache", "teeth ache", "teeth pain"): ["Acetaminophen (Tylenol)", "Ibuprofen (Advil)"],
-    ("insomnia", "sleep", "sleeping"): ["Melatonin", "Diphenhydramine (Benadryl)"],
+    ("headache","migrane","headaches","head ache","head aches","head pain","head","head pains"): ["Tylenol (Acetaminophen)", "Advil (Acetaminophen/Ibuprofen)", "Motrin IB (Ibuprofen)", "Aleve (Naproxen Sodium)", "Aspirin (Acetylsalicylic Acid)"],
+    ("cough","sore throat","coughing","coughs"): ["Advil (Ibuprofen)", "Tylenol (Acetaminophen)", "Cough drops (Honey/Mentol/Glycerin)", "Mucinex (Guaifenesin)"],
+    ("allergy","allergies","allergic"): ["Allegra (Fexofenadine)", "Zyrtec (Antihistamine)", "Benadryl (Diphenhydramine)", "Claritin (Loratadine/Antihistamine)"],
+    ("fever","sick", "cold", "sickness"): ["Tylenol (Acetaminophen)", "Advil (Ibuprofen)", "DayQuil/NyQuil (Acetaminophen)", "Alka-Seltzer Plus Cold & Flu (Acetaminophen)"],
+    ("nausea", "nauseous"): ["Pepto Bismol (Bismuth Subsalicylate)", "Emetrol (Phosphorated Carbohydrate Solution)", "Dramamine (Antihisamine)", "Nauzene (Sodium Citrate Dihydrate)"],
+    "diarrhea": ["Imodium (Loperamide)", "Pepto Bismol (Bismuth Subsalicylate)"],
+    ("constipation", "constipated","constipate"): ["MiraLAX (Polyethylene Glycol)", "Dulcolax (Bisacodyl)", "Metamucil (Pysllium Fiber)", "Stool Softeners (Emollient Laxatives)"],
+    ("heartburn","heart burn","indigestion", "indigest","sour","sour stomach"): ["Tums (Calcium Carbonate)", "Alka-Seltzer (Sodium Bicarbonate/Calcium Carbonate)", "Rolaids (Calcium Carbonate)"],
+    ("runny nose","runny"): ["Claritin (Loratadine/Antihistamine)", "Zyrtec (Antihistamine)"],
+    ("sneezing", "sneeze"): ["Claritin (Loratadine/Antihistamine)", "Zyrtec (Antihistamine)", "Allegra (Fexofenadine)"],
+    ("congestion","stuffy", "stuffed nose", "stuffy nose", "nose congestion", "congest"): ["Sudafed (Pseudoephedrine)", "Tylenol Sinus (Phenylephrine/Decongestant)", "Mucinex Sinus-Max (Phenylephrine/Decongestant)", "Sinex (Oxymetazoline)", "Vicks VapoRub (Menthol/Camphor)"],
+    ("muscle pain", "sore muscle", "sore muscles", "muscle sore", "muscle sores", "muscle", "muscles"): ["Acetaminophen (Tylenol)", "Ibuprofen (Advil)", "Advil (NSAID)"],
+    ("toothache", "tooth pain", "tooth", "teethache", "teeth ache", "teeth pain"): ["Motrin (Ibuprofen)", "Advil (Ibuprofen)", "Orajel (Benzocaine)", "Advil (NSAID)"],
+    ("insomnia", "sleep", "sleeping"): ["Melatonin (Hormone Inducer)", "ZzzQuil (Diphenhydramine)", "Benadryl (Diphenhydramine)"],
     ("cut", "cuts"): ["Antiseptic ointments (Neosporin)", "Bandages"],
     ("burn", "burns"): ["Aloe vera gel", "Hydrocortisone cream"],
     ("insect bite", "mosquito bite", "bug bite", "bugbite", "bite"): ["Hydrocortisone cream", "Antihistamines (Claritin)"],
@@ -40,12 +40,13 @@ medDict = {
     ("itch", "itchy", "scratchy"): ["Hydrocortisone cream", "Antihistamines (Claritin)"],
     ("earache", "ear pain", "ear", "ears"): ["Acetaminophen (Tylenol)", "Ibuprofen (Advil)"],
     ("dry eyes", "dry eye"): ["Artificial tears", "Lubricating eye ointments"],
-    ("stomachache", "indigestion", "stomach ache"): ["Antacids (Tums)", "Bismuth subsalicylate (Pepto-Bismol)"],
+    ("stomachache", "stomach ache"): ["Antacids (Tums)", "Bismuth subsalicylate (Pepto-Bismol)"],
     ("menstrual cramps", "period", "menstrual", "period cramp", "menstrual cramp", "cramp", "cramps"): ["Ibuprofen (Advil)", "Naproxen (Aleve)"],
     ("joint pain", "inflammation", "back pain"): ["Acetaminophen (Tylenol)", "Ibuprofen (Advil)"],
     ("fungal", "fungal infection", "fungal infections"): ["Antifungal creams (Lotrimin)", "Antifungal powders"],
     ("acne", "pimple", "pimples"): ["Benzoyl peroxide", "Salicylic acid"],
-    ("cold sore","cold sores","coldsore"): ["Docosanol (Abreva)", "Lysine supplements"]
+    ("cold sore", "cold sores", "coldsore"): ["Docosanol (Abreva)", "Lysine supplements"],
+    ("lactose", "lactose intolerance", "lactose intolerant", "intolerant"): ["Lactase supplements (Lactaid)"]
 }
 
 def recommend(symptom):
@@ -57,11 +58,10 @@ def recommend(symptom):
                 if s in symptom:
                     return "For your symptom(s), you can consider: " + ", ".join(meds) + ". " + msg
         elif sympt in symptom:
-            return "For your symptom(s), you can consider: " + ", ".join(meds) + ". " + msg
+            return "Here are some popular OTC medications you can consider: " + ", ".join(meds) + ". " + msg
     return errorMsg
         
 
 if st.button("Get Recommendation"):
     st.write("### Recommendation:")
     st.success(recommend(symptom))
-
