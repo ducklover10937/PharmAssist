bb6ed44980672df085d178d6d5d295a2766a7558
Kristie Kim <kristiekim2009@gmail.com>
2025-09-18 23:15:17 +1000

    Update PharmAssist.py

--- PharmAssist.py
+++ PharmAssist.py
@@ -10,7 +10,7 @@ st.set_page_config(
 st.title("PharmAssistant")
 st.write("Your Over-the-Counter (OTC) medication recommender")
 st.caption("Please note that PharmBot is not a substitute for professional medical advice. Always confirm with your pharmacist before purchasing an OTC medication!" \
-"Be sure to consult a healthcare provider if you are experiencing serious symptoms, are pregnant, or taking other medications.")
+" Be sure to consult a healthcare provider if you are experiencing serious symptoms, are pregnant, or taking other medications.")
 
 chat = st.text_area("Describe your symptoms, responses, or concerns:")
 
@@ -92,6 +92,7 @@ if st.button("Send"):
         st.session_state.ynRespond = recAsk
         st.write("### Recommendation:")
         st.success(recMatch)
+
 
 
 
