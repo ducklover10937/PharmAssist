cf39b4d24a8423e79ff7b4692578628887a0388c
Kristie Kim <kristiekim2009@gmail.com>
2025-09-18 22:44:28 +1000

    Update PharmAssist.py

--- PharmAssist.py
+++ PharmAssist.py
@@ -81,16 +81,14 @@ if st.button("Send"):
         if yn is True:
             st.write("### Nearby Pharmacies:")
             st.components.v1.html(
-                '<iframe src="https://www.google.com/maps/search/pharmacy+near+me/@13.4944287,144.7907148,14z?entry=ttu&g_ep=EgoyMDI1MDkxNS4wIKXMDSoASAFQAw%3D%3D" width="100%" height="500"></iframe>',
+                '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d31037.348342169684!2d144.79071477180486!3d13.494510482085245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spharmacy%20near%20me!5e0!3m2!1sko!2s!4v1758199426931!5m2!1sko!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
                 height=500
             )
         elif yn is False:
             st.write("Okay, just be sure to always consult your pharmacist or check the product label for appropriate dosages!")
         st.session_state.ynRespond = False
     else:
-        recMatch = respond(chat)
-        if recMatch:
-            st.session_state.ynRespond = True
+        recMatch, recAsk = respond(chat)
+        recAsk = st.session_state.ynRespond
         st.write("### Recommendation:")
         st.success(recMatch)
-
