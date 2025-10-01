3a960842534b6266f4d493b05bdf8e7fc35bab17
Kristie Kim <kristiekim2009@gmail.com>
2025-10-01 22:43:31 +1000

    Update PharmAssist.py

--- PharmAssist.py
+++ PharmAssist.py
@@ -124,7 +124,7 @@ if st.button("Send") and chat.strip() != "":
             st.session_state.ynRespond = recAsk
             st.session_state.chatHistory.append("Recommendation:")
             st.session_state.chatHistory.append(recMatch)
-    st.session_state.chatInput = ""
+    st.session_state["chatInput"] = ""
     st.rerun()
 
 with chatContainer:
@@ -135,5 +135,6 @@ with chatContainer:
             st.markdown("<div style='background-color: #536e70; color: #d4f1ff; text-align: left; overflow-wrap:break-word; display:inline-block; padding: 10px; max-width: 70%; border-radius: 20px;'>"+chatEntry+"</div>", unsafe_allow_html=True)
         else:
             st.markdown("<div style='background-color: #d8ebf2; color: #152e33; text-align: left; overflow-wrap:break-word; float: right;display:inline-block; padding: 10px; border-radius: 20px; max-width: 70%'>"+chatEntry+"</div>", unsafe_allow_html=True)
+
 
 
